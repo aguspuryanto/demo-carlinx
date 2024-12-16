f26b8eef4c0e990e4e8aa6f09bc77e9e6954ba08
Agus Puryanto <aguspuryanto@gmail.com>
2024-12-16 16:05:48 +0700

    update

---
+++ public/assets/js/geoapify.js
@@ -0,0 +1,15 @@
+
+    var requestOptions = {
+      method: 'GET',
+    };
+
+    $('#lokasiJemput').on('change', function () {
+      var textAddress = $(this).val();
+
+      fetch("https://api.geoapify.com/v1/geocode/autocomplete?text=" + textAddress + "&apiKey=b13dfbfe7b934de88fdf373de1f1c1c9", requestOptions)
+        .then(response => response.json())
+        .then(function (result) {
+          console.log(result);
+        })
+        .catch(error => console.log('error', error));
+    });
