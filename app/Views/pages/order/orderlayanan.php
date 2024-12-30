0b2ea7afd9bba0ab3c4c13fca1e63c8633637aba
Agus Puryanto <aguspuryanto@gmail.com>
2024-12-30 08:41:07 +0700

    update

---
+++ app/Views/pages/order/orderlayanan.php
@@ -0,0 +1,19 @@
+<?= $this->extend('layouts/main') ?>
+
+<?= $this->section('content') ?>
+
+    <!-- main page content -->
+    <div class="main-container container">
+        <div class="row">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Hitung Tarif</h4>
+                </div>
+                <div class="card-body">
+                    <?php include_once '_form.php'; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end main page content -->
+<?= $this->endSection() ?>
