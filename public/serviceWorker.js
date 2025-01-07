f68bacf92f8bf0ae3c012b2ad8616330478a9819
Agus Puryanto <aguspuryanto@gmail.com>
2025-01-07 10:33:09 +0700

    update rollback pwa

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "Carlinx-v2";
-const CACHE_ASSETS = [
+var static = "Carlinx-v2";
+var cacheassets = [
   "/splash.html",
   "/landing.html",
   "/signin.html",
@@ -12,66 +12,36 @@ const CACHE_ASSETS = [
   "/assets/js/main.js",
   "/assets/js/popper.min.js",
   "/assets/js/pwa-services.js",
-  // Add favicon assets
-  "/assets/img/favicon16.png",
-  "/assets/img/favicon32.png"
 ];
 
 self.addEventListener("install", function (event) {
-    console.log("Service Worker: Installing...");
+    console.log("Service Worker : Installed!")
 
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log("Service Worker: Caching Files");
-                return cache.addAll(CACHE_ASSETS);
-            })
-            .then(() => self.skipWaiting())
-            .catch(error => {
-                console.error("Service Worker: Caching failed", error);
-            })
+        // caches.open(static).then(function (cache) {
+        //     cache.addAll(cacheassets);
+        // }).then(function () {
+        //     return self.skipWaiting();
+        // })
+        (async() => {
+            try {
+                cache_obj = await caches.open(cache)
+                cache_obj.addAll(cacheassets)
+            }
+            catch{
+                console.log("error occured while caching...")
+                return self.skipWaiting();
+            }
+        })()
     );
 });
-
-self.addEventListener("activate", function (event) {
-    console.log("Service Worker: Activated");
-    
-    // Clean up old caches
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== CACHE_NAME) {
-                        console.log('Service Worker: Clearing Old Cache');
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener("activate", function (event) {    
 });
 
 self.addEventListener("fetch", function (event) {
     event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Return cached version or fetch new
-                return response || fetch(event.request)
-                    .then(response => {
-                        // Make copy of response
-                        const responseClone = response.clone();
-                        // Open cache
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                // Add response to cache
-                                cache.put(event.request, responseClone);
-                            });
-                        return response;
-                    })
-                    .catch(() => {
-                        // If both fail, show a generic fallback:
-                        return caches.match('/offline.html');
-                    });
-            })
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request)
+        })
     );
 });
