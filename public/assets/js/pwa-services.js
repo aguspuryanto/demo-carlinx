1e987e76462260fceff73a3c60c7686bb2533b92
Agus Puryanto <aguspuryanto@gmail.com>
2025-01-08 11:54:30 +0700

    update pwa

--- public/assets/js/pwa-services.js
+++ public/assets/js/pwa-services.js
@@ -1,13 +1,23 @@
 'use strict'
 /* PWA services worker register */
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function (event) {
-        navigator.serviceWorker
-            .register("././serviceWorker.js", {
-                scope: './'
-            })
-            .then(reg => console.log("service worker registered"))
-            .catch(err => console.log("service worker not registered"));
+// if ("serviceWorker" in navigator) {
+//     window.addEventListener("load", function (event) {
+//         navigator.serviceWorker
+//             .register("././serviceWorker.js", {
+//                 scope: './'
+//             })
+//             .then(reg => console.log("service worker registered"))
+//             .catch(err => console.log("service worker not registered"));
+//     });
+// }
+
+var BASE_URL = '<?= base_url() ?>';
+if ('serviceWorker' in navigator && navigator.onLine) {
+    navigator.serviceWorker.register( BASE_URL + 'serviceWorker.js')
+    .then((reg) => {
+        console.log('Registrasi service worker Berhasil', reg);
+    }, (err) => {
+        console.error('Registrasi service worker Gagal', err);
     });
 }
 
