d89cb1f40d0ad4204ed1c7166e2f8c6206d2a4c3
Agus Puryanto <aguspuryanto@gmail.com>
2025-01-13 15:32:05 +0700

    update manifest

--- app/Controllers/Inbox.php
+++ app/Controllers/Inbox.php
@@ -35,7 +35,7 @@ class Inbox extends BaseController
         ];
 
         if(empty($listData)) $listData = getCurl($curlOpt, $this->ipAddress . 'select_order_5.php');
-        // echo json_encode($listData); die();
+        echo json_encode($listData); die();
         if($listData['success']){
             $listData = $listData;
         }
