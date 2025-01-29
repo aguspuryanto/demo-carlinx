458a5e37c0faeafb6e6de016c736ee955e675e29
Agus Puryanto <aguspuryanto@gmail.com>
2025-01-29 20:13:39 +0700

    upate order layanan

---
+++ app/Views/pages/order/_modal_confirm_order.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="confirmModal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <a href="#" class="btn" data-bs-dismiss="modal" aria-label="Close">
+            <i class="fa fa-arrow-left"></i>
+        </a>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
