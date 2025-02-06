33ff729f511ded6f8ef574d7dee086550b16991e
Agus Puryanto <aguspuryanto@gmail.com>
2025-02-06 14:43:32 +0700

    update pengaturan detail unit

--- gitpull.sh
+++ gitpull.sh
@@ -2,4 +2,4 @@ cp -R app/Views/* /home/carlinxi/partner.carlinx.id/ci4_core/app/Views/
 cp -R app/Controllers/* /home/carlinxi/partner.carlinx.id/ci4_core/app/Controllers/
 cp -R app/Config/Routes.php /home/carlinxi/partner.carlinx.id/ci4_core/app/Config/Routes.php
 cp -R app/Helpers/my_helper.php /home/carlinxi/partner.carlinx.id/ci4_core/app/Helpers/my_helper.php
-#cp -R public/assets/* /home/carlinxi/partner.carlinx.id/assets/
+cp -R public/assets/* /home/carlinxi/partner.carlinx.id/assets/
