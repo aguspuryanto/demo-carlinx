4a57e6d173a13b32a350150f1a8090606f6c40f5
Agus Puryanto <aguspuryanto@gmail.com>
2025-02-28 08:20:38 +0700

    update wilayah

--- app/Views/pengaturan/_alert.php
+++ app/Views/pengaturan/_alert.php
@@ -3,7 +3,7 @@
         <?php if (session()->getFlashdata('error')) : ?>
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <?= session()->getFlashdata('error') ?>
-            <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
                 <span>&times;</span>
             </button>
             </div>
@@ -12,7 +12,7 @@
          <?php if (session()->getFlashdata('success')) : ?>
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 <?= session()->getFlashdata('success') ?>
-                <button type="button" class="close" data-bs-dismiss="alert" aria-label="Close">
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
                     <span>&times;</span>
                 </button>
             </div>
