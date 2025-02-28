a8b92f245b55c893170ee04a56fa1c9fbedd6175
Agus Puryanto <aguspuryanto@gmail.com>
2025-02-28 15:37:58 +0700

    update proses

--- app/Views/proses/_modal_order.php
+++ app/Views/proses/_modal_order.php
@@ -3,15 +3,16 @@
     <div class="modal fade" id="addModal" aria-labelledby="addModalLabel" aria-hidden="false" aria-modal="true">
     <div class="modal-dialog">
         <div class="modal-content">
-        <div class="modal-header">
-            <a href="#" class="btn" data-bs-dismiss="modal" aria-label="Close">
-                <i class="fa fa-arrow-left"></i> Detail Order
-            </a>
-        </div>
-        <div class="modal-body">
-            ...
-        </div>
-        <div class="modal-footer">
+            <div class="modal-header">
+                <a href="#" class="btn" data-bs-dismiss="modal" aria-label="Close">
+                    <i class="fa fa-arrow-left"></i> Detail Order
+                </a>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+            </div>
         </div>
     </div>
     </div>
