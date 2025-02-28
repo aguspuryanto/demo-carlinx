1e9afebc512577f9c0cb882926bddb7caadcb9c2
Agus Puryanto <aguspuryanto@gmail.com>
2025-02-28 09:38:43 +0700

    update proses

--- app/Controllers/Proses.php
+++ app/Controllers/Proses.php
@@ -27,10 +27,12 @@ class Proses extends BaseController
          * caller optional
          * kd_member required
          */
+        // echo json_encode($this->session->get('user'));
 
         $listData   = [];
+        $listUser   = [];
         $curlOpt    = [
-            'caller' => 'AKTIF', // default. INBOX, AKTIF, RIWAYAT
+            'caller' => 'INBOX', // default. INBOX, AKTIF, RIWAYAT
             'kd_member' => $this->session->get('user')['kode']
         ];
 
@@ -39,16 +41,46 @@ class Proses extends BaseController
         // 5 = konfirmasi bayar (upload bukti transfer)
         // 6 = tolak by rental; 7 = batal by rental; 8 = batal by pemesan
         // 9 = pembayaran diterima (order selesai - ke menu Proses)
-        $listStatus = [
-            '1' => 'Baru',
+
+        // sisi pemesan
+        // Stat : 1 = Menunggu
+        // Stat : 4 = Butuh Tindakan
+        // Stat : 5 = Menunggu
+        // Stat : 6 = Ditolak
+        // Stat : 7 = Rental Batal
+        // Stat : 8 = Kedaluwarsa
+        // Stat : 9 = Selesai
+
+        // sisi rental
+        // Stat : 1 = Order Baru
+        // Stat : 4 = Menunggu
+        // Stat : 5 = Tunggu Pembayaran
+        // Stat : 6 = Ditolak
+        // Stat : 7 = Rental Batal
+        // Stat : 8 = Pemesan Batal
+        // Stat : 9 = Selesai
+
+        $listStatus = [];
+        $listStatus_pemesan = [
+            '1' => 'Menunggu',
             '2' => 'Diterima',
             '3' => 'Data Plgn',
-            '4' => 'Data Driver + Invoice',
-            '5' => 'Konfirmasi Bayar',
+            '4' => 'Butuh Tindakan',
+            '5' => 'Menunggu',
             '6' => 'Ditolak', //'Tolak By Rental',
-            '7' => 'Batal By Rental',
-            '8' => 'Pemesan Batal', //'Batal By Pemesan',
+            '7' => 'Rental Batal',
+            '8' => 'Kedaluwarsa', //'Batal By Pemesan',
             '9' => 'Selesai', //'Pembayaran Diterima'
+        ];
+
+        $listStatus_rental = [
+            '1' => 'Order Baru',
+            '4' => 'Menunggu',
+            '5' => 'Tunggu Pembayaran',
+            '6' => 'Ditolak',
+            '7' => 'Rental Batal',
+            '8' => 'Pemesan Batal',
+            '9' => 'Selesai'
         ];
 
         // grp_penyewa
@@ -66,18 +98,40 @@ class Proses extends BaseController
             // '5' => 'Paket',
         ];
 
-        if(empty($listData)) $listData = getCurl($curlOpt, $this->ipAddress . 'select_order_5.php');
-        // echo json_encode($listData); die();
+        $dataLogin = [
+            'usernm' => $this->session->get('user')['username'],
+            'passwd' => $this->session->get('user')['password']
+        ];
+        // if(empty($listUser)) $listUser = getCurl($dataLogin, $this->ipAddress . 'login_user.php');
+        if(empty($listUser)) $listUser = ($this->session->get('user'));
+        // echo json_encode($listUser); die();
 
+        if(empty($listData)) $listData = getCurl($curlOpt, $this->ipAddress . 'select_order_5.php');
+        
+        if ($listData === null) {
+            // Handle JSON decode error
+            echo "Error decoding JSON";
+            die();
+        }
+        
         $newlistData = [];
-        if($listData['success']=='1'){
+        if ($listData['success'] == '1') {
             foreach ($listData['result_list_order'] as $item) {
                 // echo $item['stat'];
-                if($item['stat']=='9') $newlistData[] = $item;
+                if(in_array($item['stat'], ['1', '2', '3', '4', '5'])) $newlistData[] = $item;
             }
         }
-
         // echo json_encode($newlistData);
+
+        if($listData['success']){
+            $listData = $listData;
+            
+            $is_vendor = ($listData['result_list_order'][0]['kode_rental'] == $listUser['kd_rental']) ? true : false;
+            $is_pemesan = ($listData['result_list_order'][0]['kode_rental'] != $listUser['kd_rental']) ? true : false;
+
+            if($is_vendor) $listStatus = $listStatus_rental;
+            if($is_pemesan) $listStatus = $listStatus_pemesan;
+        }
 
         return view('proses/index', [
             'title' => 'Proses',
@@ -85,7 +139,8 @@ class Proses extends BaseController
             'listGroup' => $listGroup,
             'listOrder' => $listOrder,
             'newlistData' => $newlistData,
-            'listData' => $listData
+            'listData' => $listData,
+            'listUser' => $listUser
         ]);
     }
 }
