bf9f8b349a6b2e011e64047120c6aaeb57782762
Agus Puryanto <aguspuryanto@gmail.com>
2025-03-04 13:45:11 +0700

    update prod report

--- gitpull.sh
+++ gitpull.sh
@@ -2,5 +2,5 @@ cp -R app/Views/* /home/carlinxi/partner.carlinx.id/ci4_core/app/Views/
 cp -R app/Controllers/* /home/carlinxi/partner.carlinx.id/ci4_core/app/Controllers/
 cp -R app/Config/Routes.php /home/carlinxi/partner.carlinx.id/ci4_core/app/Config/Routes.php
 cp -R app/Helpers/my_helper.php /home/carlinxi/partner.carlinx.id/ci4_core/app/Helpers/my_helper.php
-# cp -R public/assets/* /home/carlinxi/partner.carlinx.id/assets/
+cp -R public/assets/js/* /home/carlinxi/partner.carlinx.id/assets/js
 # cp -R public/serviceWorker.js /home/carlinxi/partner.carlinx.id/
